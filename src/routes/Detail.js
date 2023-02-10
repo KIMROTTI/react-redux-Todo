2edc2f5a3abbaedd7bceb34692fba4f9b41f7471
kimrotti <andy91052990@gmail.com>
2023-02-10 11:52:29 +0900

    feat : dynamic pages

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,24 @@
-const Detail = () => {
-  return <div>Detail</div>;
+import { type } from "@testing-library/user-event/dist/type";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+
+const Detail = ({ toDos }) => {
+  const params = useParams();
+  console.log(typeof params.id);
+  console.log(toDos);
+
+  const toDo = toDos.find((toDo) => toDo.id === parseInt(params.id));
+
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <h5>Created at: {toDo?.id}</h5>
+    </>
+  );
 };
 
-export default Detail;
+function mapStateToProps(state, ownProps) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Detail);
